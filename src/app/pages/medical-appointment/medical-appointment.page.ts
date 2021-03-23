70f85bf6a296568856be6396a70f44f67d662aba
Ramon Cibas <ramon.cibas@hotmail.com>
2021-03-23 18:31:33 -0300

    add appointment page

---
+++ src/app/pages/medical-appointment/medical-appointment.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-medical-appointment',
+  templateUrl: './medical-appointment.page.html',
+  styleUrls: ['./medical-appointment.page.scss'],
+})
+export class MedicalAppointmentPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
