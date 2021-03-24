d503a59ef7d678a0504031056028d291e0c16d80
Ramon Cibas <ramon.cibas@hotmail.com>
2021-03-24 19:36:24 -0300

    doctor interface

--- src/app/interfaces/doctor.ts
+++ src/app/interfaces/doctor.ts
@@ -1,2 +1,9 @@
 export interface Doctor {
+    id?: string;
+    name?: string;
+    decription?: string;
+    specialty?: string;
+    picture?: string;
+    price?: string;
+    createdAt?: string;    
 }
