84f042c9311e7120e33628b1fc01e1990d471aa8
Ramon Cibas <ramon.cibas@hotmail.com>
2021-03-24 19:38:27 -0300

    false data to test the interface

--- src/app/pages/medical-appointment/medical-appointment.page.ts
+++ src/app/pages/medical-appointment/medical-appointment.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Appointment } from 'src/app/interfaces/appointment';
+import { AppointmentService } from 'src/app/services/appointment.service';
 
 @Component({
   selector: 'app-medical-appointment',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicalAppointmentPage implements OnInit {
 
-  constructor() { }
+  // Fake data
+  private appointments = [
+    {
+      name: 'Dr. Marcio',
+      specialty: 'Clinico Geral',
+      picture:
+        'https://images.unsplash.com/photo-1543486958-d783bfbf7f8e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c2VsZmllfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+      description: 'Consulta de rotina.',
+      createdAt: new Date(),
+      price: null,
+    },
+  ];
+  
+  // private appointments =  new Array<Appointment>();
+  // private appointmentsSubscription: Subscription;
 
-  ngOnInit() {
-  }
+  // constructor( private appointmentsService: AppointmentService ) {
+  //   this.appointmentsSubscription = this.appointmentsService.getAppointments().subscribe(data => {
+  //     this.appointments = data;
+  //   });
+  // }
 
+  ngOnInit() {}
+
+  // // Destroy listen
+  // ngOnDestroy() {
+  //   this.appointmentsSubscription.unsubscribe();
+  // }
 }
