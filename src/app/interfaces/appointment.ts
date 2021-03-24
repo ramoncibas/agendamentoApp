48019250b0a18f027ee753536fbfcc88e0366db2
Ramon Cibas <ramon.cibas@hotmail.com>
2021-03-24 19:35:54 -0300

    appointment interface

---
+++ src/app/interfaces/appointment.ts
@@ -0,0 +1,12 @@
+export interface Appointment {
+    id?: string;
+    doctorId?: string;
+    specialty?: string;
+    description?: string;
+
+    // When the user makes an appointment
+    userId?: string;
+    healthInsurance?: boolean;
+    price?: string;
+    createdAt?: number;
+}
