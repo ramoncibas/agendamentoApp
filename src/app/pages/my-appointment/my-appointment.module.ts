683497319a60081352e9e680c66be8c7b44b2db9
Ramon Cibas <ramon.cibas@hotmail.com>
2021-03-25 19:02:20 -0300

    add modal

---
+++ src/app/pages/my-appointment/my-appointment.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { MyAppointmentPageRoutingModule } from './my-appointment-routing.module';
+
+import { MyAppointmentPage } from './my-appointment.page';
+import { MyAppointmentModalPage } from '../modals/myappointment/myappointment.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    MyAppointmentPageRoutingModule
+  ],
+  declarations: [MyAppointmentPage, MyAppointmentModalPage]
+})
+export class MyAppointmentPageModule {}
