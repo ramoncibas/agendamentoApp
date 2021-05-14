e05540de3b9977135bb62bbe4a7176b4c30a3575
Ramon Cibas <ramon.cibas@hotmail.com>
2021-05-13 22:35:43 -0300

    passing values ​​to the modal

--- src/app/pages/my-appointment/my-appointment.page.ts
+++ src/app/pages/my-appointment/my-appointment.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-import { MyAppointmentModalPage } from '../modals/myappointment/myappointment.page'
+import { MyAppointmentModalPage } from '../modals/modal-appointment/modal-appointment.page'
 
 @Component({
   selector: 'app-my-appointment',
@@ -14,17 +14,55 @@ export class MyAppointmentPage implements OnInit {
   ngOnInit() {
   }
 
-  async presentModal() {
-    const dateHj = new Date();
+  // Fake data
+  showModal(e) {
+    if (e == 'pills') {
+      console.log(e)
+      const data = {    
+        name: 'Paracetamol',        
+        secondName: '12mg',
+        dayAt: new Date()
+      }
+      this.presentModal(data)
+
+    } else if (e == 'vaccine') {
+      console.log(e)
+      const data = {
+        name: 'Covid-19',
+        secondName: 'CoronaVac',
+        dayAt: new Date()
+      }
+      this.presentModal(data)
+
+    } else if (e == 'check-up') {
+      const data = {    
+        name: 'Consulta de Rotina',
+        secondName: 'Dr. Yang',
+        dayAt: new Date()
+      }
+      this.presentModal(data)
+
+    } else  {
+      console.log(e)
+      const data = {    
+        name: 'Dr. Yang',
+        doctorImg: 'https://images.unsplash.com/photo-1543486958-d783bfbf7f8e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c2VsZmllfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+        secondName: 'Clinico Geral',
+        dayAt: new Date()
+      }
+      this.presentModal(data)
+    }
+
+  }
+
+  async presentModal(modalParam) { 
+    console.log(modalParam)
     const modal = await this.modalControll.create({
       component: MyAppointmentModalPage,
       cssClass: 'my-custom-class',
       swipeToClose: true,
       componentProps: {
-        'firtName': 'Dr. Yang',
-        'doctorImg': 'https://images.unsplash.com/photo-1543486958-d783bfbf7f8e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c2VsZmllfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
-        'doctorSpeciality': 'Clinico Geral',
-        'dayAt': dateHj
+        modalParam
       }
     });
     return await modal.present();
