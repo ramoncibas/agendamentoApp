668e08c983f3e67ed67c31b154a7c153c10d1661
Ramon Cibas <ramon.cibas@hotmail.com>
2021-06-10 22:06:54 -0300

    add my profile page and enabling the logout function

---
+++ src/app/pages/my-profile/my-profile.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { User } from 'src/app/interfaces/user';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-my-profile',
+  templateUrl: './my-profile.page.html',
+  styleUrls: ['./my-profile.page.scss'],
+})
+export class MyProfilePage implements OnInit {
+
+  public userProfile: User = {};
+  
+  constructor(private authServices: AuthService, private navgation: NavController) { }
+
+  ngOnInit() {}
+
+  async loggoutBtn() {
+    try {
+      await this.authServices.loggout();
+      this.navgation.navigateForward("/login")
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
