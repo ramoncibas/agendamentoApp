ac4a4553e6968c352623a7708e1c81625a90ec4c
Ramon Cibas <ramon.cibas@hotmail.com>
2021-06-10 22:08:56 -0300

    some updates

--- src/app/pages/my-appointment/my-appointment.module.ts
+++ src/app/pages/my-appointment/my-appointment.module.ts
@@ -7,7 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { MyAppointmentPageRoutingModule } from './my-appointment-routing.module';
 
 import { MyAppointmentPage } from './my-appointment.page';
-import { MyAppointmentModalPage } from '../modals/modal-appointment/modal-appointment.page';
+import { MyAppointmentModalPage } from '../../components/Modals/modal-appointment/modal-appointment.page';
 
 @NgModule({
   imports: [
--- src/app/pages/my-appointment/my-appointment.page.ts
+++ src/app/pages/my-appointment/my-appointment.page.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-import { MyAppointmentModalPage } from '../modals/modal-appointment/modal-appointment.page'
+import { MyAppointmentModalPage } from '../../components/Modals/modal-appointment/modal-appointment.page'
 
 @Component({
   selector: 'app-my-appointment',
   templateUrl: './my-appointment.page.html',
   styleUrls: ['./my-appointment.page.scss'],
 })
-export class MyAppointmentPage implements OnInit {  
+export class MyAppointmentPage implements OnInit {
 
   constructor(public modalControll: ModalController) { }
 
@@ -19,7 +19,7 @@ export class MyAppointmentPage implements OnInit {
     if (e == 'pills') {
       console.log(e)
       const data = {    
-        name: 'Paracetamol',        
+        name: 'Medicalmento: Paracetamol',        
         secondName: '12mg',
         dayAt: new Date()
       }
@@ -28,16 +28,16 @@ export class MyAppointmentPage implements OnInit {
     } else if (e == 'vaccine') {
       console.log(e)
       const data = {
-        name: 'Covid-19',
-        secondName: 'CoronaVac',
+        name: 'Vacinando contra: Covid-19',
+        secondName: 'Vacina: CoronaVac',
         dayAt: new Date()
       }
       this.presentModal(data)
 
     } else if (e == 'check-up') {
       const data = {    
-        name: 'Consulta de Rotina',
-        secondName: 'Dr. Yang',
+        name: 'Exame: Consulta de Rotina',
+        secondName: 'Medico: Dr. Yang',
         dayAt: new Date()
       }
       this.presentModal(data)
@@ -45,9 +45,9 @@ export class MyAppointmentPage implements OnInit {
     } else  {
       console.log(e)
       const data = {    
-        name: 'Dr. Yang',
+        name: 'Medico: Dr. Yang',
         doctorImg: 'https://images.unsplash.com/photo-1543486958-d783bfbf7f8e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c2VsZmllfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
-        secondName: 'Clinico Geral',
+        secondName: 'Especialidade: Clinico Geral',
         dayAt: new Date()
       }
       this.presentModal(data)
