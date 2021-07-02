953d208b562c9253f709034332b15ec1c21fb844
Ramon Cibas <ramon.cibas@hotmail.com>
2021-07-02 19:41:49 -0300

    fake data

--- src/app/pages/medical-appointment/medical-appointment.page.ts
+++ src/app/pages/medical-appointment/medical-appointment.page.ts
@@ -24,6 +24,33 @@ export class MedicalAppointmentPage implements OnInit {
       price: null,
       timeAvaliable:'2021-05-01T15:43:40.394Z'
     },
+    {
+      name: 'Dr. Tanga',
+      specialty: 'Clinico Geral',
+      picture:
+        'https://images.unsplash.com/photo-1543486958-d783bfbf7f8e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c2VsZmllfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+      description: 'Consulta de rotina.',
+      price: null,
+      timeAvaliable:'2021-05-01T15:43:40.394Z'
+    },
+    {
+      name: 'Dr. Silva',
+      specialty: 'Clinico Geral',
+      picture:
+        'https://images.unsplash.com/photo-1543486958-d783bfbf7f8e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c2VsZmllfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+      description: 'Consulta de rotina.',
+      price: null,
+      timeAvaliable:'2021-05-01T15:43:40.394Z'
+    },
+    {
+      name: 'Dr. Mauricio',
+      specialty: 'Clinico Geral',
+      picture:
+        'https://images.unsplash.com/photo-1543486958-d783bfbf7f8e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8c2VsZmllfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
+      description: 'Consulta de rotina.',
+      price: null,
+      timeAvaliable:'2021-05-01T15:43:40.394Z'
+    },
   ];
 
   
