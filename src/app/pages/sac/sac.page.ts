31f59fc399ccd577297021ed419457aa0502ef50
Ramon Cibas <ramon.cibas@hotmail.com>
2021-07-13 21:56:56 -0300

    action sheet and modal chat added

--- src/app/pages/sac/sac.page.ts
+++ src/app/pages/sac/sac.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController, ModalController } from '@ionic/angular';
+import { ModalChatPage } from '../../components/Modals/modal-chat/modal-chat.page';
 
 @Component({
   selector: 'app-sac',
@@ -6,10 +8,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sac.page.scss'],
 })
 export class SacPage implements OnInit {
+  constructor(
+    private actionSheetController: ActionSheetController,
+    private modalControll: ModalController
+  ) { }
 
-  constructor() { }
+  ngOnInit() { }
 
-  ngOnInit() {
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Compartilhar',
+      cssClass: 'my-custom-class',
+      buttons: [
+        {
+          text: 'WhatsApp',
+          icon: 'logo-whatsapp',
+          handler: () => {
+            console.log('WhatsApp clicked');
+          },
+        },
+        {
+          text: 'Facebook',
+          icon: 'logo-facebook',
+          handler: () => {
+            console.log('Facebook clicked');
+          },
+        },
+        {
+          text: 'Instagram',
+          icon: 'logo-instagram',
+          handler: () => {
+            console.log("Instagram clicked");
+          },
+        },
+        {
+          text: 'LinkedIn',
+          icon: 'logo-linkedin',
+          handler: () => {
+            console.log('Linkedin clicked');
+          },
+        },
+        {
+          text: 'Email',
+          icon: 'mail',
+          handler: () => {
+            console.log('Email clicked');
+          },
+        },
+        {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          },
+        },
+      ],
+    });
+    await actionSheet.present();
+
+    const { role } = await actionSheet.onDidDismiss();
+    console.log(role);
   }
 
+  async presentModal(data) {
+    const modal = await this.modalControll.create({
+      component: ModalChatPage,
+      cssClass: 'my-custom-class',
+      swipeToClose: true,
+      componentProps: {
+        data,
+      },
+    });
+    return await modal.present();
+  }
 }
